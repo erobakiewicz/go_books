740a32ae121ee8c18f3dc9060194a1c954655856
erobakiewicz <eliasz.robakiewicz@laboratorium.ee>
2022-01-31 19:36:04 +0100

    basic layout

--- books/forms.py
+++ books/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit
 
 KEY_WORDS = (
     ("intitle:", "title"),
@@ -9,18 +11,22 @@ KEY_WORDS = (
 
 
 class BookImportForm(forms.Form):
-    searched_phrase = forms.CharField(help_text="What are you looking for?")
-    key_word = forms.ChoiceField(choices=KEY_WORDS)
+
+    searched_phrase = forms.CharField(label="What are you looking for?")
+    key_word = forms.ChoiceField(choices=KEY_WORDS, label='Choose: title, author, subject or isbn')
 
 
 class BookSearchForm(forms.Form):
-    title = forms.CharField(required=False)
-    author = forms.CharField(required=False)
+
+    title = forms.CharField(required=False, label="Title")
+    author = forms.CharField(required=False, label="Author")
     date_published_start = forms.DateField(
         required=False,
-        widget=forms.widgets.DateInput(attrs={'type': 'date'})
+        widget=forms.widgets.DateInput(attrs={'type': 'date'}),
+        label="Date published: from"
     )
     date_published_end = forms.DateField(
         required=False,
-        widget=forms.widgets.DateInput(attrs={'type': 'date'})
+        widget=forms.widgets.DateInput(attrs={'type': 'date'}),
+        label="Date published: to"
     )
